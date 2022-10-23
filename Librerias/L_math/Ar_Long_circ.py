742c35842a44bd50e0342cade79331cd9a6da7b4
John Jaime <jhonjaimemadrid02@gmail.com>
2022-10-22 19:22:38 -0500

    Agregar carpeta Libreria

---
+++ Librerias/L_math/Ar_Long_circ.py
@@ -0,0 +1,8 @@
+import math as m
+
+r = float(input('Ingresar valor del radio: '))
+A = m.pi*r**2
+L = 2*m.pi*r
+
+print('El area del circulo tiene un valor de :', r)
+print('La longitud tiene un valor de:', L)
